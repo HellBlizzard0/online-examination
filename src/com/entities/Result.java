0c45662316e47d1ff6c5373183a1f9bc3c312ecb
HellBlizzard <hellblizzard5@gmail.com>
2022-01-24 11:21:37 +0300

    Added View Results for the Administrator

--- src/com/entities/Result.java
+++ src/com/entities/Result.java
@@ -14,20 +14,22 @@ import org.hibernate.annotations.NamedQuery;
 
 @SuppressWarnings("serial")
 @NamedQueries({
-	@NamedQuery(
-			name = "student_fetchAllStudentResult",
-			query =  "from Result s "
-					+ "where s.uid = :P_UID"),
-	@NamedQuery(
-			name = "result_getResultByStudentIdAndExamId",
-			query = "from Result s "
-					+ "where s.uid = :P_UID "
-					+ "and s.eid = :P_EID"),
+		@NamedQuery(
+				name = "result_fetchAllResult",
+				query = "from Result s "),
+		@NamedQuery(
+				name = "student_fetchAllStudentResult",
+				query = "from Result s "
+						+ "where s.uid = :P_UID"),
+		@NamedQuery(
+				name = "result_getResultByStudentIdAndExamId",
+				query = "from Result s "
+						+ "where s.uid = :P_UID "
+						+ "and s.eid = :P_EID"),
 })
 
 @Entity
 @Table(name = "result")
-
 
 public class Result implements Serializable {
 	public Result(int rid, int eid, int uid, int score) {
@@ -38,75 +40,54 @@ public class Result implements Serializable {
 		this.score = score;
 	}
 
-
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "RID")
 	private int rid;
-	
+
 	@Column(name = "EID")
 	private int eid;
-	
+
 	@Column(name = "UID")
 	private int uid;
-	
+
 	@Column(name = "Score")
 	private int score;
-	
-	
-	
+
 	public Result() {
-		
+
 	}
-
-
 
 	public int getRid() {
 		return rid;
 	}
 
-
-
 	public void setRid(int rid) {
 		this.rid = rid;
 	}
-
-
 
 	public int getEid() {
 		return eid;
 	}
 
-
-
 	public void setEid(int eid) {
 		this.eid = eid;
 	}
-
-
 
 	public int getUid() {
 		return uid;
 	}
 
-
-
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
-
-
 
 	public int getScore() {
 		return score;
 	}
 
-
-
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
-	
+
 }
--- src/com/entities/Student.java
+++ src/com/entities/Student.java
@@ -1,4 +1,5 @@
 package com.entities;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -12,24 +13,25 @@ import org.hibernate.annotations.NamedQueries;
 
 @SuppressWarnings("serial")
 @NamedQueries({
-	@NamedQuery(
-			name = "student_fetchAllStudents",
-			query =  "from Student s"),
-	@NamedQuery(
-			name = "student_fetchStudentByLoginCredintials",
-			query = "from Student s "
-					+ "where s.username = :P_USERNAME "
-					+ "and s.password = :P_PASSWORD"),
-	@NamedQuery(
-			name = "student_fetchStudentById",
-			query = "from Student s "
-					+ "where s.id = :P_TO_DATE_STRING")
+		@NamedQuery(
+				name = "student_fetchAllStudents",
+				query = "from Student s"),
+		@NamedQuery(
+				name = "student_fetchStudentByLoginCredintials",
+				query = "from Student s "
+						+ "where s.username = :P_USERNAME "
+						+ "and s.password = :P_PASSWORD"),
+		@NamedQuery(
+				name = "student_fetchStudentById",
+				query = "from Student s "
+						+ "where s.id = :P_ID "),
+
 })
 
 @Entity
 @Table(name = "student")
 public class Student implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
@@ -71,46 +73,57 @@ public class Student implements Serializable {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getLevel() {
 		return level;
 	}
+
 	public void setLevel(int level) {
 		this.level = level;
 	}
-
-
 
 }
